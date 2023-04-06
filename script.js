f02d5dcb753deaa0016767abdf605e0cabd4c06e
David Forrester <david.o.forrester@gmail.com>
2023-04-06 21:31:38 +1000

    optimising the functions using chatGPT

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Function to return the computers choice
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-  let computerChoice = "";
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  let computerChoice;
 
-  if (randomNumber == 1) {
-    computerChoice = "rock";
-  } else if (randomNumber == 2) {
-    computerChoice = "paper";
-  } else {
-    computerChoice = "scissors";
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+    default:
+      computerChoice = "";
   }
 
   return computerChoice;
@@ -16,83 +22,46 @@ function getComputerChoice() {
 
 // Function to play a game
 function playRound(playerSelection, computerSelection) {
-  let playerSelectionLower = playerSelection.toLowerCase();
-  let computerSelectionLower = computerSelection.toLowerCase();
-  let result = "";
+  const choices = {
+    rock: { win: "scissors", lose: "paper" },
+    paper: { win: "rock", lose: "scissors" },
+    scissors: { win: "paper", lose: "rock" },
+  };
 
-  if (playerSelectionLower == "rock" && computerSelectionLower == "rock") {
-    result = "You Draw, you both picked Rock";
-  } else if (
-    playerSelectionLower == "rock" &&
-    computerSelectionLower == "paper"
-  ) {
-    result = "You Lose, Paper beats Rock";
-  } else if (
-    playerSelectionLower == "rock" &&
-    computerSelectionLower == "scissors"
-  ) {
-    result = "You Win, Rock beats Scissors";
-  } else if (
-    playerSelectionLower == "paper" &&
-    computerSelectionLower == "rock"
-  ) {
-    result = "You Win, Paper beats Rock";
-  } else if (
-    playerSelectionLower == "paper" &&
-    computerSelectionLower == "paper"
-  ) {
-    result = "You Draw, you both picked Paper";
-  } else if (
-    playerSelectionLower == "paper" &&
-    computerSelectionLower == "Scissors"
-  ) {
-    result = "You Lose, Scissors beats paper";
-  } else if (
-    playerSelectionLower == "scissors" &&
-    computerSelectionLower == "rock"
-  ) {
-    result = "You Lose, Rock beats Scissors";
-  } else if (
-    playerSelectionLower == "scissors" &&
-    computerSelectionLower == "paper"
-  ) {
-    result = "You Win, Scissors beats paper";
-  } else if (
-    playerSelectionLower == "scissors" &&
-    computerSelectionLower == "scissors"
-  ) {
-    result = "You Draw, you both picked Scissors";
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (choices[playerSelection].win === computerSelection) {
+    return `You Win, ${playerSelection} beats ${computerSelection}`;
+  } else if (choices[playerSelection].lose === computerSelection) {
+    return `You Lose, ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You Draw, you both picked ${playerSelection}`;
   }
-
-  return result;
 }
 
 // Plays 5 games of rock paper scissors
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let result = "";
-    const playerSelection = window.prompt("What is your selection?: ");
-    const computerSelection = getComputerChoice();
-    result = playRound(playerSelection, computerSelection);
-    console.log(result);
-
-    if (result[4] == "W") {
-      playerScore += 1;
-    } else if (result[4] == "L") {
-      computerScore += 1;
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      let result = "";
+      const [playerSelection, computerSelection] = [prompt("What is your selection?: "), getComputerChoice()];
+      result = playRound(playerSelection, computerSelection);
+      console.log(`${result}`);
+  
+      result[4] == "W" ? playerScore++ : result[4] == "L" ? computerScore++ : null;
     }
+  
+    const resultString = playerScore > computerScore
+      ? `You win with a score of ${playerScore} to ${computerScore}`
+      : playerScore < computerScore
+      ? `You lose with a score of ${playerScore} to ${computerScore}`
+      : `You draw with a score of ${playerScore} to ${computerScore}`;
+  
+    console.log(resultString);
   }
-
-  if (playerScore > computerScore) {
-    console.log(`You win with a score of ${playerScore} to ${computerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log(`You lose with a score of ${playerScore} to ${computerScore}`);
-  } else {
-    console.log(`You draw with a score of ${playerScore} to ${computerScore}`);
-  }
-}
+  
 
 game();
